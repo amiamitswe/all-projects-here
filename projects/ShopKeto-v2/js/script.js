d453812a2ce83d2e997c784c67586ffcc842a908
amiamitswe <amiamitswe@gmail.com>
2020-04-02 19:57:19 +0600

    add new section

--- projects/ShopKeto-v2/js/script.js
+++ projects/ShopKeto-v2/js/script.js
@@ -1,3 +1,33 @@
 $('.sk-product__main-img').slick({
   
 });
+
+
+
+$(document).ready(function() {
+  // add active calss at nav
+  $(".sk-product__info-btn").click(function() {
+    $(".sk-product__info-btn").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  // show current clicked data
+  $("#sk-product__info-list li button").on("click", function(e) {
+    // prevent default behaviour of the anchor tag
+    e.preventDefault();
+    // save the data attribute for the anchor tag that was clicked
+    var page = $(this).data("page");
+
+    // find the current 'page' element that doesn't have the class .hide -- this is a css selector
+    $("#sk-product__info-details .sk-product__item-info:not('.display-none')")
+      .stop()
+      .fadeOut("fast", function() {
+        // adds .hide to the element that was showing after it has faded out
+        $(this).addClass("display-none");
+        // remove hidden class from element with the same data attribute as the anchor tag
+        $('#sk-product__info-details .sk-product__item-info[data-page="' + page + '"]')
+          .fadeIn("slow")
+          .removeClass("display-none");
+      });
+  });
+});
