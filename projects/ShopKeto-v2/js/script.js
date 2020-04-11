998c9e0155cce8d03755d408dc57929cbc5bed95
amiamitswe <amiamitswe@gmail.com>
2020-04-11 18:01:30 +0600

    some fixing

--- projects/ShopKeto-v2/js/script.js
+++ projects/ShopKeto-v2/js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
   // jquery for slick slider
-  $('.sk-product__main-img').slick({
+  $('.sk-product__main-img-test').slick({
     autoplay: false,
     dots: false,
     draggable: true
@@ -34,5 +34,44 @@ $(document).ready(function() {
           .fadeIn("slow")
           .removeClass("display-none");
       });
+  });
+
+
+// --------------------------------------- 
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: false,
+    adaptiveHeight: true,
+    infinite: false,
+   useTransform: true,
+    speed: 300
+  });
+ 
+  $('.slider-nav')
+    .on('init', function(event, slick) {
+      $('.slider-nav .slick-slide.slick-current').addClass('active');
+    })
+    .slick({
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      dots: false,
+      focusOnSelect: false,
+      infinite: false
+    });
+ 
+  $('.slider-for').on('afterChange', function(event, slick, currentSlide) {
+    $('.slider-nav').slick('slickGoTo', currentSlide);
+    var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+    $('.slider-nav .slick-slide.active').removeClass('active');
+    $(currrentNavSlideElem).addClass('active');
+  });
+ 
+  $('.slider-nav').on('click', '.slick-slide', function(event) {
+    event.preventDefault();
+    var goToSingleSlide = $(this).data('slick-index');
+ 
+    $('.slider-for').slick('slickGoTo', goToSingleSlide);
   });
 });
