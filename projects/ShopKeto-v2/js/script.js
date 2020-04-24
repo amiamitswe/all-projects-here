2404c3bb6f2d8d729f9d3ca19ad2708d7d6f62cd
amiamitswe <amiamitswe@gmail.com>
2020-04-24 21:23:54 +0600

    change carasole style and other styles

--- projects/ShopKeto-v2/js/script.js
+++ projects/ShopKeto-v2/js/script.js
@@ -2,43 +2,41 @@
 $(document).ready(function() {
 
   // jquery for slick slider
-  $('.sk-main__banner-slider').slick({
-    autoplay: false,
-    dots: true,
-    draggable: true,
-    autoplay: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          dots: false,
-        }
-      }
-    ]
-  });
+  // $('.sk-main__banner-slider').slick({
+  //   autoplay: false,
+  //   dots: true,
+  //   draggable: true,
+  //   autoplay: false,
+  //   centerMode: true,
+  //   centerPadding: '95px',
+
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         dots: false,
+  //         centerPadding: '11px',
+  //       }
+  //     }
+  //   ]
+  // });
 
   
   // jquery for content navigation
-  // add active calss at nav
   $(".sk-product__info-btn").click(function() {
     $(".sk-product__info-btn").removeClass("active");
     $(this).addClass("active");
   });
 
-  // show current clicked data
   $("#sk-product__info-list li button").on("click", function(e) {
-    // prevent default behaviour of the anchor tag
     e.preventDefault();
-    // save the data attribute for the anchor tag that was clicked
     var page = $(this).data("page");
 
-    // find the current 'page' element that doesn't have the class .hide -- this is a css selector
     $("#sk-product__info-details .sk-product__item-info:not('.display-none')")
       .stop()
       .fadeOut("fast", function() {
-        // adds .hide to the element that was showing after it has faded out
         $(this).addClass("display-none");
-        // remove hidden class from element with the same data attribute as the anchor tag
+
         $('#sk-product__info-details .sk-product__item-info[data-page="' + page + '"]')
           .fadeIn("slow")
           .removeClass("display-none");
@@ -47,20 +45,21 @@ $(document).ready(function() {
 
 
 // --------------------------------------- 
-  $('.slider-for').slick({
+// slick slider for porduce details 
+  $('.sk-product-slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     fade: false,
     adaptiveHeight: true,
     infinite: false,
-   useTransform: true,
+    useTransform: true,
     speed: 300
   });
  
-  $('.slider-nav')
+  $('.sk-product-slider-nav')
     .on('init', function(event, slick) {
-      $('.slider-nav .slick-slide.slick-current').addClass('active');
+      $('.sk-product-slider-nav .slick-slide.slick-current').addClass('active');
     })
     .slick({
       slidesToShow: 2,
@@ -70,17 +69,50 @@ $(document).ready(function() {
       infinite: false
     });
  
-  $('.slider-for').on('afterChange', function(event, slick, currentSlide) {
-    $('.slider-nav').slick('slickGoTo', currentSlide);
-    var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
-    $('.slider-nav .slick-slide.active').removeClass('active');
+  $('.sk-product-slider-for').on('afterChange', function(event, slick, currentSlide) {
+    $('.sk-product-slider-nav').slick('slickGoTo', currentSlide);
+    var currrentNavSlideElem = '.sk-product-slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+    $('.sk-product-slider-nav .slick-slide.active').removeClass('active');
     $(currrentNavSlideElem).addClass('active');
   });
  
-  $('.slider-nav').on('click', '.slick-slide', function(event) {
+  $('.sk-product-slider-nav').on('click', '.slick-slide', function(event) {
     event.preventDefault();
     var goToSingleSlide = $(this).data('slick-index');
  
-    $('.slider-for').slick('slickGoTo', goToSingleSlide);
+    $('.sk-product-slider-for').slick('slickGoTo', goToSingleSlide);
   });
+});
+
+
+
+// ----------------------------------
+
+$('.sk-main__banner-slider').slick({
+  centerMode: true,
+  dots: true,
+  centerPadding: '0px',
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 1
+      }
+      },
+    {
+    breakpoint: 768,
+    settings: {
+      arrows: true,
+      centerMode: true,
+      centerPadding: '0px',
+      slidesToShow: 1
+    }
+    }
+  ]
 });
